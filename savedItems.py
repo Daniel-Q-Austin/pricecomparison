94731e7f9bcc572fedb8779ad62b6b9414876c2f
Daniel-Q-Austin <67240093+Daniel-Q-Austin@users.noreply.github.com>
2020-07-28 17:24:22 -0300

    Update savedItems.py

--- savedItems.py
+++ savedItems.py
@@ -1,8 +1,24 @@
+"""
+Module Name:   SavedItems.py
+Coded by:      Daniel Austin
+Date coded:    July 16, 2020.
+Date approved: July 27, 2020
+Approved by:   Daniel Austin
 
+Desctiption:   This module handles the backend of the 'saved items' portion of PriceAid. 
+               That is, when a user wants to save an item for later, this module is 
+               used to store the information about that item in a database.
 
-#This module will run the 'saved item' portion of PriceAid
-#Daniel Austin, Summer of 2020
+Arguments:
+    arg connection: A connection object to the relational database
+    arg cursor: The cursor within the database object.
 
+Files accesed:
+    saveditems - A relational database.
+
+Testing File:  savedItems_tests.docx
+
+"""
 import os
 import mysql.connector
 
@@ -16,10 +32,10 @@ cursor = connection.cursor()
 print("Connected")
 
 
-def addNewItem(item, url, price): 
+def addNewItem(item, url, imageUrl, price): 
     #Adds a saved item
-    sql = 'INSERT INTO saved_table (Name, URL, Price) VALUES (%s, %s, %s)'
-    val = (item, url, price)
+    sql = 'INSERT INTO saved_table (Name, URL, ImageUrl, Price) VALUES (%s, %s, %s, %s)'
+    val = (item, url, imageUrl, price)
     try:
         cursor.execute(sql, val)
     except mysql.connector.errors.IntegrityError:
@@ -32,7 +48,7 @@ def removeItem(item):
     cursor.execute("DELETE FROM saved_table WHERE Name = '{}'".format(item))
     count = cursor.rowcount
     if (count == 0):
-        return "Item not in cart"
+        return "Item does not exist"
     connection.commit()
     return "Item deleted successfully"
 
